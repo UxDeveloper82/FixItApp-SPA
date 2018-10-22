23ee553792f56b3b8a47bf0bc45ea3d244d27ceb
electronicsmf <electronicsmf@aol.com>
2018-10-22 12:45:04 -0400

    End Section 8

--- FixItApp.API/Data/DataContext.cs
+++ FixItApp.API/Data/DataContext.cs
@@ -9,5 +9,7 @@ namespace FixItApp.API.Data
 
         public DbSet<Value> Values { get; set; }  
         public DbSet<User> Users { get; set; } 
+
+        public DbSet<Photo> Photos { get; set; }
     }
 }
