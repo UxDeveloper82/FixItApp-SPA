7e239e1978417da1aa83975142c5b7f40ee7cd44
electronicsmf <electronicsmf@aol.com>
2018-10-23 09:05:15 -0400

    End Of Section 9

--- fixitApp-SPA/src/app/routes.ts
+++ fixitApp-SPA/src/app/routes.ts
@@ -1,10 +1,13 @@
+import { MemberListResolver } from './_resolvers/member-list.resolver.';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { HomeComponent } from './home/home.component';
 import { MyBlogComponent } from './my-blog/my-blog.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { Routes } from '@angular/router';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -13,7 +16,10 @@ export const appRoutes: Routes = [
        runGuardsAndResolvers: 'always',
        canActivate: [AuthGuard],
        children: [
-        { path: 'members', component: MemberListComponent },
+        { path: 'members', component: MemberListComponent,
+            resolve: { users: MemberListResolver}},
+        { path: 'members/:id', component: MemberDetailComponent,
+          resolve: {user: MemberDetailResolver}},
         { path: 'messages', component: MessagesComponent },
         { path: 'lists', component: ListsComponent },
         { path: 'blog', component: MyBlogComponent },
